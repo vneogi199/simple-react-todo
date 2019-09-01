d9633541abb204be43472d0ab66c5110f2adfd68
Vinit Neogi <vneogi199@gmail.com>
2019-09-01 20:22:39 +0530

    Updates

---
+++ precache-manifest.ef13ac946a6a2de372ce634317212e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fc325ac84e96c456b693993cd4f224",
+    "url": "/simple-react-todo/index.html"
+  },
+  {
+    "revision": "63837cccad7d4e56fd91",
+    "url": "/simple-react-todo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "84db37a2cb3797f6f6bf",
+    "url": "/simple-react-todo/static/css/main.bce64d13.chunk.css"
+  },
+  {
+    "revision": "63837cccad7d4e56fd91",
+    "url": "/simple-react-todo/static/js/2.1ea115d8.chunk.js"
+  },
+  {
+    "revision": "84db37a2cb3797f6f6bf",
+    "url": "/simple-react-todo/static/js/main.a71b9404.chunk.js"
+  },
+  {
+    "revision": "ec247d464fa9dfb2a804",
+    "url": "/simple-react-todo/static/js/runtime~main.7a99856a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
